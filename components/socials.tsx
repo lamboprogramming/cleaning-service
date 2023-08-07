f5223876862068db0e0e4dcd2eb93efb563f6f8e
Lamboprogramming <gillespielamar@gmail.com>
2023-08-07 18:55:22 -0400

    more bugs when building

--- components/socials.tsx
+++ components/socials.tsx
@@ -10,20 +10,20 @@ const socials = () => {
     <div className='flex flex-row justify-between p-6 text-6xl tracking-wide sm:justify-evenly'>
       
       <Link href='https://www.thumbtack.com/ca/san-diego/house-cleaning/champion-cleaning-llc/service/457493064825454607'>
-      <div className='cursor-pointer '><h3><SiThumbtack /></h3>
-      
+      <div className='cursor-pointer '>
+        <SiThumbtack />
       </div>
       </Link>
 
       <Link href='https://www.facebook.com/championcleaning4/'>
-      <div className='cursor-pointer'> <h3><FaFacebookSquare /></h3>
-     
+      <div className='cursor-pointer'>
+         <FaFacebookSquare />
        </div>
       </Link>
 
       <Link href='https://www.instagram.com/championcleaningllc/?hl=en'>
-      <div className='cursor-pointer'> <h3> <FaInstagramSquare /></h3>
-      
+      <div className='cursor-pointer'>
+         <FaInstagramSquare />
        </div>
       </Link>
     </div>
